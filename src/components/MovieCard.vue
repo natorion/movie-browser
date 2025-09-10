73c26cdfe9710bf33fdbd1d614e379033e6c20bc
Michael Hablich <hablich@google.com>
2025-09-10 16:28:36 +0200

    Add a randomizer for loading posters

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="movie-card" @click="onCardClick">
-    <img :src="movie.poster_path ? 'https://image.tmdb.org/t/p/w200' + movie.poster_path : 'https://via.placeholder.com/200x300?text=No+Image'" :alt="movie.title + ' poster'">
+    <img :src="movie.poster_path ? 'https://image.tmdb.org/t/p/w200' + getRandomizedPosterPath(movie.poster_path) : 'https://via.placeholder.com/200x300?text=No+Image'" :alt="movie.title + ' poster'">
     <h3>{{ movie.title }}</h3>
     <p>Release Date: {{ movie.release_date }}</p>
     <p>Rating: {{ movie.vote_average }} / 10</p>
@@ -19,6 +19,16 @@ export default {
   methods: {
     onCardClick() {
       this.$emit('movie-clicked', this.movie);
+    },
+    getRandomizedPosterPath(posterPath) {
+      console.log('MovieCard getRandomizedPosterPath called with:', posterPath);
+      // Randomly make the URL invalid with roughly 50% chance
+      if (Math.random() < 0.5) {
+        console.log('MovieCard making URL invalid');
+        return '/invalid-path' + posterPath;
+      }
+      console.log('MovieCard returning original poster path');
+      return posterPath;
     }
   }
 }
